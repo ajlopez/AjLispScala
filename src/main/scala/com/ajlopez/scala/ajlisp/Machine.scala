c73357a6b92c1940938adfb0caa701b800a832da
ajlopez <webmaster@ajlopez.com>
2014-03-30 16:01:59 -0300

    New files

---
+++ src/main/scala/com/ajlopez/scala/ajlisp/Machine.scala
@@ -0,0 +1,7 @@
+
+package com.ajlopez.scala.ajlisp
+
+class Machine {
+    def evaluate(a: Any) = a
+}
+
---
+++ src/test/scala/com/ajlopez/scala/ajlisp/MachineSpec.scala
@@ -0,0 +1,13 @@
+
+package com.ajlopez.scala.ajlisp
+
+import org.scalatest._
+
+class MachineSpec extends org.scalatest.FlatSpec {
+    "A Machine" should "evaluate simple values" in {
+        val machine = new com.ajlopez.scala.ajlisp.Machine()
+        assert(machine.evaluate(1) == 1)
+        assert(machine.evaluate("foo") == "foo")
+        assert(machine.evaluate(null) == null)
+    }
+}
